2cf760beb0439fd72fc2f7a489428b29c3e8d92e
Mikel Bravo <me@mklbravo.net>
2023-09-12 18:55:43 +0200

    feat(gitsigns): install and configure plugin

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,20 @@
+--[[
+  Super fast git decorations implemented purely in Lua.
+  See: https://github.com/lewis6991/gitsigns.nvim
+]]
+
+return {
+  "lewis6991/gitsigns.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    current_line_blame = true,
+    signs = {
+      add = { text = "▎" },
+      change = { text = "▎" },
+      delete = { text = "" },
+      topdelete = { text = "" },
+      changedelete = { text = "▎" },
+      untracked = { text = "▎" },
+    },
+  },
+}
