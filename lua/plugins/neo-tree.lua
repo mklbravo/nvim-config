8e31d842c01738702f6a86a7d08b98f9dd5990a0
Mikel Bravo <me@mklbravo.net>
2023-09-12 18:59:33 +0200

    feat(neo-tree): Use focus insted of toggle

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -15,7 +15,7 @@ return {
     {
       "<leader>fe",
       function()
-        require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+        require("neo-tree.command").execute({ focus = true, dir = vim.loop.cwd() })
       end,
       desc = "Explorer NeoTree (cwd)",
     },
