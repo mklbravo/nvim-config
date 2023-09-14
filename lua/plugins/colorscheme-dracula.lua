c8592119bcb4d73a0ff492fff15f012ed19a08ba
Mikel Bravo <me@mklbravo.net>
2023-09-14 11:45:16 +0200

    feat(colorscheme): update default colors

--- lua/plugins/colorscheme-dracula.lua
+++ lua/plugins/colorscheme-dracula.lua
@@ -5,12 +5,26 @@
 
 return {
   "Mofiqul/dracula.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
   init = function() 
     -- Set colorscheme on plugin init
     vim.cmd.colorscheme("dracula")
-  end
+  end,
+  lazy = false,
+  opts = {
+    colors = {
+      bg = "#22212C",
+      comment = "#7970A9",
+      cyan = "#80FFEA",
+      fg = "#F8F8F2",
+      green = "#8AFF80",
+      orange = "#FFCA80",
+      pink = "#FF80BF",
+      purple = "#9580FF",
+      red = "#FF9580",
+      selection = "#454158",
+      yellow = "#FFFF80",
+    },
+  },
+  priority = 1000,
 }
 
