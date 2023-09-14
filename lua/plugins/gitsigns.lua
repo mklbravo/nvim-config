f6b356fe272468a64f319b930ac2f932d6217d38
Mikel Bravo <me@mklbravo.net>
2023-09-14 11:46:23 +0200

    fix(gitsigns): use default signs

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -11,8 +11,6 @@ return {
     signs = {
       add = { text = "▎" },
       change = { text = "▎" },
-      delete = { text = "" },
-      topdelete = { text = "" },
       changedelete = { text = "▎" },
       untracked = { text = "▎" },
     },
