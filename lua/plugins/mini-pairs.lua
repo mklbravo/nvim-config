86bf293b7c326ea4b1d1a5bb4d93c3f8bdd390ce
Mikel Bravo <me@mklbravo.net>
2023-09-15 12:13:35 +0200

    feat(mini-pairs): require plugin

---
+++ lua/plugins/mini-pairs.lua
@@ -0,0 +1,11 @@
+--[[
+ Neovim Lua plugin to automatically manage character pairs.
+ See: https://github.com/echasnovski/mini.pairs
+]]
+
+return {
+  'echasnovski/mini.pairs',
+  config = true,
+  version = '*',
+}
+
