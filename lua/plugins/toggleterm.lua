ef52fc90472556bc787eac9c085e270295da374f
Mikel Bravo <me@mklbravo.net>
2023-09-15 15:46:55 +0200

    feat(toggleterm): require plugin

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,13 @@
+--[[
+ Plugin to persist and toggle multiple terminals during an editing session
+ See: https://github.com/akinsho/toggleterm.nvim
+--]]
+
+return {
+  'akinsho/toggleterm.nvim',
+  opts = {
+    open_mapping = [[<c-`>]],
+  },
+  version = '*',
+}
+
