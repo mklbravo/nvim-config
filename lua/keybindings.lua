e33dc09bfdb73caea4ee762ccc04eda0a1935409
Mikel Bravo <me@mklbravo.net>
2023-09-22 13:56:01 +0200

    feat(keymap): add keymap

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -4,4 +4,7 @@ vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 
+-- Move selected lines up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
 
