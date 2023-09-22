3f126a4c4c088c1f896fd4dca9c7e9ff2b650835
Mikel Bravo <me@mklbravo.net>
2023-09-22 14:31:10 +0200

    fix(treesitter): initialize configuration

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,9 +6,18 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
+  config = function (_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+  end,
   opts = {
+    additional_vim_regex_highlighting = false,
+    auto_install = true,
     ensure_installed = {
       'lua',
-    }
+    },
+    highlight = {
+      enable = true
+    },
+    sync_install = false,
   },
 }
