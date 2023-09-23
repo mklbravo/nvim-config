32dddc8c06e6ee0312851f71d3656342f1d0adb3
Mikel Bravo <me@mklbravo.net>
2023-09-23 13:12:58 +0200

    feat(mason): require package

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,9 @@
+--[[
+  Portable package manager. Easily install and manage LSP servers, DAP servers, linters, and formatters.
+  See: https://github.com/williamboman/mason.nvim
+]]
+
+return {
+  "williamboman/mason.nvim",
+  config = true,
+}
