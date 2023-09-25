78e884ccfd8f19dd6888e3d72004ef460ea810d7
Mikel Bravo <me@mklbravo.net>
2023-09-25 20:09:13 +0200

    feat(mason): require mason-lspconfig

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,21 +1,29 @@
 --[[
   Portable package manager. Easily install and manage LSP servers, DAP servers, linters, and formatters.
   See: https://github.com/williamboman/mason.nvim
+  See: https://github.com/williamboman/mason-lspconfig.nvim
 ]]
 
 return {
   "williamboman/mason.nvim",
   dependencies = {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
+    "williamboman/mason-lspconfig.nvim",
   },
   config = function(_, opts)
     require("mason").setup(opts)
 
     require("mason-tool-installer").setup({
       ensure_installed = {
+        -- LSP,
+        "lua-language-server",
+        -- Formatters
         "stylua",
       },
       run_on_start = true,
     })
+
+    require("mason-lspconfig").setup({})
   end,
+  opts = true,
 }
