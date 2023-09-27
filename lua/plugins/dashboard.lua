f696fe2177ccf4b7b8bce72859da9336a2786aa1
Mikel Bravo <me@mklbravo.net>
2023-09-27 10:57:11 +0200

    feat(dashboard): require plugin

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,14 @@
+--[[
+  Vim dashboard
+  See: https://github.com/nvimdev/dashboard-nvim
+]]
+
+return {
+  "glepnir/dashboard-nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  event = "VimEnter",
+  opts = {
+  },
+}
