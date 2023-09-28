bddaa5bba8cc53de8168312aae0384e934ad19eb
Mikel Bravo <mikel.bravo@hks-systeme.de>
2023-09-28 22:15:14 +0200

    feat(comment): require plugin

---
+++ lua/plugins/comment.lua
@@ -0,0 +1,12 @@
+--[[
+  Smart and Powerful commenting plugin for neovim
+  See: https://github.com/numToStr/Comment.nvim
+]]
+
+return {
+  "numToStr/Comment.nvim",
+  opts = {
+    -- add any options here
+  },
+  lazy = false,
+}
