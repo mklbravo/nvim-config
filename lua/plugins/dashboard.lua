c6a844f07da592d1fc6819f4757dfcd9bb944272
Mikel Bravo <me@mklbravo.net>
2023-10-09 13:25:24 +0200

    WIP

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,14 +1,28 @@
 --[[
   Vim dashboard
-  See: https://github.com/nvimdev/dashboard-nvim
+  See: https://github.com/goolord/alpha-nvim
 ]]
 
 return {
-  "glepnir/dashboard-nvim",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  event = "VimEnter",
+  "goolord/alpha-nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {
+    layout = {
+      { type = "padding", val = 1 },
+      {
+        type = "text",
+        val = {
+          [[ _____ ______   ___  __    ___       ___      ___ ___  _____ ______       ]],
+          [[|\   _ \  _   \|\  \|\  \ |\  \     |\  \    /  /|\  \|\   _ \  _   \     ]],
+          [[\ \  \\\__\ \  \ \  \/  /|\ \  \    \ \  \  /  / | \  \ \  \\\__\ \  \  ]],
+          [[ \ \  \\|__| \  \ \   ___  \ \  \    \ \  \/  / / \ \  \ \  \\|__| \  \  ]],
+          [[  \ \  \    \ \  \ \  \\ \  \ \  \____\ \    / /   \ \  \ \  \    \ \  \ ]],
+          [[   \ \__\    \ \__\ \__\\ \__\ \_______\ \__/ /     \ \__\ \__\    \ \__\]],
+          [[    \|__|     \|__|\|__| \|__|\|_______|\|__|/       \|__|\|__|     \|__|]],
+        },
+        opts = { hl = "AlphaCol2", position = "center" },
+      },
+      { type = "padding", val = 1 },
+    },
   },
 }
