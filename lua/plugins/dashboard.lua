4865c25b8c91830fa66fcf65808f643285d26285
Mikel Bravo <me@mklbravo.net>
2023-10-17 13:15:57 +0200

    feat: add basic dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -6,23 +6,30 @@
 return {
   "goolord/alpha-nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function(_, opts)
+    local alpha = require("alpha")
+    local dashboardTheme = require("alpha.themes.dashboard")
+
+    dashboardTheme.section.header.val = opts.header
+    dashboardTheme.section.buttons.val = {
+      dashboardTheme.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
+      dashboardTheme.button("f", "󰈞  > Find file", ":Telescope find_files<CR>"),
+      dashboardTheme.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
+      dashboardTheme.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h<CR>"),
+      dashboardTheme.button("q", "󰗼  > Quit NVIM", ":qa<CR>"),
+    }
+
+    alpha.setup(dashboardTheme.opts)
+  end,
   opts = {
-    layout = {
-      { type = "padding", val = 1 },
-      {
-        type = "text",
-        val = {
-          [[ _____ ______   ___  __    ___       ___      ___ ___  _____ ______       ]],
-          [[|\   _ \  _   \|\  \|\  \ |\  \     |\  \    /  /|\  \|\   _ \  _   \     ]],
-          [[\ \  \\\__\ \  \ \  \/  /|\ \  \    \ \  \  /  / | \  \ \  \\\__\ \  \  ]],
-          [[ \ \  \\|__| \  \ \   ___  \ \  \    \ \  \/  / / \ \  \ \  \\|__| \  \  ]],
-          [[  \ \  \    \ \  \ \  \\ \  \ \  \____\ \    / /   \ \  \ \  \    \ \  \ ]],
-          [[   \ \__\    \ \__\ \__\\ \__\ \_______\ \__/ /     \ \__\ \__\    \ \__\]],
-          [[    \|__|     \|__|\|__| \|__|\|_______|\|__|/       \|__|\|__|     \|__|]],
-        },
-        opts = { hl = "AlphaCol2", position = "center" },
-      },
-      { type = "padding", val = 1 },
+    header = {
+      [[ _____ ______   ___  __    ___       ___      ___ ___  _____ ______      ]],
+      [[|\   _ \  _   \|\  \|\  \ |\  \     |\  \    /  /|\  \|\   _ \  _   \    ]],
+      [[\ \  \\\__\ \  \ \  \/  /|\ \  \    \ \  \  /  / | \  \ \  \\\__\ \  \   ]],
+      [[ \ \  \\|__| \  \ \   ___  \ \  \    \ \  \/  / / \ \  \ \  \\|__| \  \  ]],
+      [[  \ \  \    \ \  \ \  \\ \  \ \  \____\ \    / /   \ \  \ \  \    \ \  \ ]],
+      [[   \ \__\    \ \__\ \__\\ \__\ \_______\ \__/ /     \ \__\ \__\    \ \__\]],
+      [[    \|__|     \|__|\|__| \|__|\|_______|\|__|/       \|__|\|__|     \|__|]],
     },
   },
 }
