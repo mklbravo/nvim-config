1ca19ad0404693910b3f438bcd86acc65cf480cb
Mikel Bravo <me@mklbravo.net>
2023-10-27 12:22:49 +0200

    feat(trouble): add key mappings

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -13,4 +13,58 @@ return {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
   },
+  keys = function()
+    local trouble = require("trouble")
+
+    return {
+      {
+        "<leader>xx",
+        function()
+          trouble.toggle()
+        end,
+        mode = "n",
+        desc = "[Trouble] Toggle panel",
+      },
+      {
+        "<leader>xw",
+        function()
+          trouble.toggle("workspace_diagnostics")
+        end,
+        mode = "n",
+        desc = "[Trouble] Toggle workspace diagnostics",
+      },
+      {
+        "<leader>xd",
+        function()
+          trouble.toggle("document_diagnostics")
+        end,
+        mode = "n",
+        desc = "[Trouble] Toggle document diagnostics",
+      },
+      {
+        "<leader>xq",
+        function()
+          trouble.toggle("quickfix")
+        end,
+        mode = "n",
+        desc = "[Trouble] Toggle quickfix",
+      },
+      {
+        "<leader>xl",
+        function()
+          trouble.toggle("loclist")
+        end,
+        mode = "n",
+        desc = "[Trouble] Toggle loclist",
+      },
+      {
+        "gR",
+        function()
+          trouble.toggle("lsp_references")
+        end,
+        mode = "n",
+        desc = "[Trouble] Toggle LSP references",
+      },
+    }
+  end,
 }
