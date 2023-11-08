6351ec9785ea64bccfdc7e411058e29498511a88
Mikel Bravo <me@mklbravo.net>
2023-11-08 16:41:12 +0100

    feat(mini-bufremove): install package

---
+++ lua/plugins/mini-bufremove.lua
@@ -0,0 +1,10 @@
+--[[
+  Neovim Lua plugin to remove buffers.
+  See: https://github.com/echasnovski/mini.bufremove
+]]
+
+return {
+  "echasnovski/mini.bufremove",
+  config = true,
+  version = "*",
+}
