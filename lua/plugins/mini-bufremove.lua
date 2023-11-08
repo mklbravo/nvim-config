ac84d75c05553b4f5ca7a3f0d68ad497be738ee3
Mikel Bravo <me@mklbravo.net>
2023-11-08 16:41:34 +0100

    feat(mini-bufremove): add key bindings

--- lua/plugins/mini-bufremove.lua
+++ lua/plugins/mini-bufremove.lua
@@ -7,4 +7,35 @@ return {
   "echasnovski/mini.bufremove",
   config = true,
   version = "*",
+  keys = function()
+    local bufremove_delete = require("mini.bufremove").delete
+
+    return {
+      {
+        "<leader>bd",
+        function()
+          if vim.bo.modified ~= true then
+            bufremove_delete(0)
+            return
+          end
+
+          local choice = vim.fn.confirm(("Save changes to %q?"):format(vim.fn.bufname()), "&Yes\n&No\n&Cancel")
+          if choice == 1 then -- Yes
+            vim.cmd.write()
+            bufremove_delete(0)
+          elseif choice == 2 then -- No
+            bufremove_delete(0, true)
+          end
+        end,
+        desc = "Delete Buffer",
+      },
+      {
+        "<leader>bD",
+        function()
+          bufremove_delete(0, true)
+        end,
+        desc = "Delete Buffer (Force)",
+      },
+    }
+  end,
 }
