fb3bf42eca384169e94315059aed6a12452fceb0
Mikel Bravo <me@mklbravo.net>
2024-01-08 11:26:53 +0100

    feat(autopairs): install and configure package

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,10 @@
+--[[
+  Autopairs for neovim written by lua
+  See: https://github.com/windwp/nvim-autopairs
+]]
+
+return {
+  "windwp/nvim-autopairs",
+  event = "InsertEnter",
+  opts = true,
+}
