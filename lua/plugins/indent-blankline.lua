76246d3f161d97f9f5ebfd5d628026c74224dc7d
Mikel Bravo <me@mklbravo.net>
2024-01-11 09:16:20 +0100

    feat(indent-blankline): install package

---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,10 @@
+--[[
+  Indent guides for NeoVim
+  See: https://github.com/lukas-reineke/indent-blankline.nvim
+]]
+
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  opts = {},
+}
