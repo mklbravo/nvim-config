d615e43cd1786174a8650f12dbb192d41aae672a
Mikel Bravo <me@mklbravo.net>
2024-02-09 17:02:21 +0100

    feat(dap): add configuration file

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,31 @@
+--[[
+  Debug Adapter Protocol client implementation for Neovim.
+  See: https://github.com/mfussenegger/nvim-dap
+]]
+
+return {
+  "mfussenegger/nvim-dap",
+  config = function()
+    local sign = vim.fn.sign_define
+
+    sign("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = "" })
+    sign("DapBreakpointCondition", { text = "●", texthl = "DapBreakpointCondition", linehl = "", numhl = "" })
+    sign("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl = "", numhl = "" })
+  end,
+  keys = function()
+    local dap = require("dap")
+    return {
+      { "<leader>db", dap.toggle_breakpoint, mode = "n", desc = "toggle breakpoint", silent = true },
+      -- Generated code
+      -- { "<leader>dc", dap.continue(), mode="n", desc="continue" },
+      -- { "<leader>dd", dap.step_over(), mode="n", desc="step over" },
+      -- { "<leader>di", dap.step_into(), mode="n", desc="step into" },
+      -- { "<leader>do", dap.step_out(), mode="n", desc="step out" },
+      -- { "<leader>dr", dap.repl.open(), mode="n", desc="open repl" },
+      -- { "<leader>ds", dap.stop(), mode="n", desc="stop" },
+      -- { "<leader>dt", dap.toggle_breakpoint(), mode="n", desc="toggle breakpoint" },
+      -- { "<leader>du", dap.up(), mode="n", desc="up" },
+      -- { "<leader>dx", dap.down(), mode="n", desc="down"
+    }
+  end,
+}
