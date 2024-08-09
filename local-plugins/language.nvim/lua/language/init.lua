1b722b4e7f7ee37430b69c1e7bb859bb49af7a16
Mikel Bravo <me@mklbravo.net>
2024-08-09 10:58:19 +0200

    feat(language-plugin): add UI for enable/disable languages

--- local-plugins/language.nvim/lua/language/init.lua
+++ local-plugins/language.nvim/lua/language/init.lua
@@ -4,6 +4,7 @@ function M.setup(config)
   vim.api.nvim_command("echo 'Hello from init.lua'")
   print("My local plugin is loaded!")
   print("Config: ", config)
+  vim.api.nvim_create_user_command("LanguageUI", "lua require('language.ui').open()", { nargs = 0 })
 end
 
 return M
