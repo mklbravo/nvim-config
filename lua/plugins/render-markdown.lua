6a12e31fb15bc3068b82d588276805200a93e55c
Mikel Bravo <me@mklbravo.net>
2025-06-16 15:52:26 +0200

    feat: user render-markdown.nvim

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,9 @@
+--[[
+  Plugin to improve viewing Markdown files in Neovim
+  See: https://github.com/MeanderingProgrammer/render-markdown.nvim
+]]
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  config = true,
+  ft = { "markdown", "codecompanion" },
+}
