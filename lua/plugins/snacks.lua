80600ca39ca32aed3c6fefab3c16a7b5cc2e4b0f
Mikel Bravo <me@mklbravo.net>
2025-10-15 13:24:17 +0200

    refactor(snacks): extracs keys into variables

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,6 +3,49 @@
   See: https://github.com/folke/snacks.nvim
 ]]
 ---@diagnostic disable: undefined-global
+-- Buffer key mappings
+local buffer_keys = {
+  {
+    "<leader>bd",
+    function()
+      Snacks.bufdelete()
+    end,
+    desc = "Close current buffer",
+  },
+  {
+    "<leader>ba",
+    function()
+      Snacks.bufdelete.all()
+    end,
+    desc = "Close all buffers",
+  },
+  {
+    "<leader>bo",
+    function()
+      Snacks.bufdelete.other()
+    end,
+    desc = "Close all buffers except current",
+  },
+}
+
+-- Lazygit key mappings
+local lazygit_keys = {
+  {
+    "<leader>G",
+    function()
+      Snacks.lazygit.open()
+    end,
+    desc = "Open lazygit",
+  },
+  {
+    "<leader>gf",
+    function()
+      Snacks.lazygit.log_file()
+    end,
+    desc = "Open lazygit log for current file",
+  },
+}
+
 return {
   "folke/snacks.nvim",
   lazy = false,
@@ -11,41 +54,5 @@ return {
     lazygit = { enabled = true },
     scroll = { enabled = true },
   },
-  keys = {
-    {
-      "<leader>bd",
-      function()
-        Snacks.bufdelete()
-      end,
-      desc = "Close current buffer",
-    },
-    {
-      "<leader>ba",
-      function()
-        Snacks.bufdelete.all()
-      end,
-      desc = "Close all buffers",
-    },
-    {
-      "<leader>bo",
-      function()
-        Snacks.bufdelete.other()
-      end,
-      desc = "Close all buffers except current",
-    },
-    {
-      "<leader>G",
-      function()
-        Snacks.lazygit.open()
-      end,
-      desc = "Open lazygit",
-    },
-    {
-      "<leader>gf",
-      function()
-        Snacks.lazygit.log_file()
-      end,
-      desc = "Open lazygit log for current file",
-    },
-  },
+  keys = vim.tbl_extend("force", buffer_keys, lazygit_keys),
 }
