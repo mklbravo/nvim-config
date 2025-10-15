a66f4eeb7041187a8c38436201265a0bf7193949
Mikel Bravo <me@mklbravo.net>
2025-10-15 11:03:58 +0200

    feat: add snacks package

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,11 @@
+--[[
+  A collection of QoL plugins for Neovim
+  See: https://github.com/folke/snacks.nvim
+]]
+
+return {
+  "folke/snacks.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {},
+}
