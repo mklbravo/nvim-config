5fa0c87539a38fdb7d40774bbbe00578d4414b89
Mikel Bravo <me@mklbravo.net>
2025-10-15 11:30:12 +0200

    feat(snacks): use bufel functions

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,10 +2,33 @@
   A collection of QoL plugins for Neovim
   See: https://github.com/folke/snacks.nvim
 ]]
-
+---@diagnostic disable: undefined-global
 return {
   "folke/snacks.nvim",
   lazy = false,
   priority = 1000,
   opts = {},
+  keys = {
+    {
+      "<leader>bd",
+      function()
+        Snacks.bufdelete()
+      end,
+      desc = "Close current buffer",
+    },
+    {
+      "<leader>ba",
+      function()
+        Snacks.bufdelete.all()
+      end,
+      desc = "Close all buffers",
+    },
+    {
+      "<leader>bo",
+      function()
+        Snacks.bufdelete.other()
+      end,
+      desc = "Close all buffers except current",
+    },
+  },
 }
