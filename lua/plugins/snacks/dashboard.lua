d70b1590e72ce32cba5ac6276bcea4f47d562098
Mikel Bravo <me@mklbravo.net>
2025-10-17 13:54:23 +0200

    feat(snacks-dashboard): migrate to snacks dashboard

--- lua/plugins/snacks/dashboard.lua
+++ lua/plugins/snacks/dashboard.lua
@@ -2,5 +2,26 @@
 return {
   opts = {
     enabled = true,
+    sections = {
+      { section = "header" },
+      { section = "keys", gap = 1, padding = 1 },
+      { section = "startup" },
+      { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+      { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+      {
+        pane = 2,
+        icon = " ",
+        title = "Git Status",
+        section = "terminal",
+        enabled = function()
+          return Snacks.git.get_root() ~= nil
+        end,
+        cmd = "git --no-pager diff --stat -B -M -C",
+        height = 5,
+        padding = 1,
+        ttl = 5 * 60,
+        indent = 3,
+      },
+    },
   },
 }
