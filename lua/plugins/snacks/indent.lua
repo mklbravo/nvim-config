a04f85e6d17a64aa4dfd909f6d2633adbd14d6bf
Mikel Bravo <me@mklbravo.net>
2025-10-22 16:29:33 +0200

    feat(snacks-indent): enable plugin

---
+++ lua/plugins/snacks/indent.lua
@@ -0,0 +1,11 @@
+--[[
+  Visualize indent guides and scopes based on treesitter or indent.
+  See: https://github.com/folke/snacks.nvim/blob/main/docs/indent.md
+]]
+
+---@diagnostic disable: undefined-global
+---@type SnacksConfig
+return {
+  opts = {
+    enabled = true,
+}
