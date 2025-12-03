97539a5fb8f95f28b3a81f67736d933951d88d64
Mikel Bravo <me@mklbravo.net>
2025-12-03 11:37:21 +0100

    feat(code-companion): update commit workflow instructions for clarity and detail

--- lua/plugins/code-companion.lua
+++ lua/plugins/code-companion.lua
@@ -42,12 +42,16 @@ return {
             content = [[
               ### Steps to follow:
                1. Please get current directory git diff.
-               2. Analyze the git diff and determine the type of change (feat, fix, docs, style, refactor, test, chore).
-               3. Based on your analysis, generate a Conventional Commit message that accurately describes the changes made.
-               4. Ensure the commit message follows the Conventional Commits format: <type>(<scope>): <description>.
-               5. Provide only the commit message as the output, without any additional explanations or formatting.
-               6. Stage the changes
-               7. Commit the changes with the generated commit message.
+               2. Decide if the changes needs to be commites in a single commit or multiple commits.
+               3. If multiple commits are needed, break down the changes accordingly and generate separate commit messages for each.
+               4. For each required commit, follow these steps:
+                 1. Analyze and determine the type of change (feat, fix, docs, style, refactor, test, chore).
+                 2. Based on your analysis, generate a Conventional Commit message that accurately describes the changes made.
+                 3. Ensure the commit message follows the Conventional Commits format: <type>(<scope>): <description>.
+                 4. Make sure the description is concise and informative.
+                 5. Provide the commit message and description as the output, with  additional explanation if necessary.
+                 6. Stage the changes.
+                 7. Commit the changes with the generated commit message.
 
               ### Tools you can use:
                 - @{cmd_runner}: A tool that allows you to run shell commands. Use this to get the git diff and to stage and commit changes.
