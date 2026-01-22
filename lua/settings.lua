c5b0a40c82f57ca029b4b51262dadd5e46813baf
Mikel Bravo <me@mklbravo.net>
2026-01-22 14:54:33 +0100

    fix(settings): correct typos and improve comments in Neovide settings section
    Corrects spelling errors and improves clarity in comments related to Neovide configuration in settings.lua.

--- lua/settings.lua
+++ lua/settings.lua
@@ -11,7 +11,7 @@ vim.g.maplocalleader = " " -- Set local leader key to space
 vim.o.cursorline = true -- Highlight the text line of the cursor
 vim.o.clipboard = "unnamedplus" -- Use system clipboard for all operations
 vim.o.fillchars = "eob: " -- Set space as eob(end of buffer) character
-vim.o.mouse = "a" -- Enable trackpad/mouse suppont in all modes
+vim.o.mouse = "a" -- Enable trackpad/mouse support in all modes
 vim.o.number = true -- Show line numbers in the gutter
 vim.o.swapfile = false -- Disable swapfile creation
 vim.o.termguicolors = true -- Enable true color support in the terminal
@@ -31,15 +31,15 @@ vim.o.expandtab = true -- Convert tabs to spaces
 vim.o.shiftwidth = 2 -- Number of spaces to use for each step of (auto)indent
 vim.o.smartindent = true -- Enable smart auto-indentation for new lines
 vim.o.tabstop = 2 -- Number of spaces that a <Tab> character represents
--- }}}
 
 -- ------------------------------------------------
--- Newovide Settings
+-- Neovide Settings
 -- ------------------------------------------------
 if vim.g.neovide then
   vim.o.guifont = "MonaspiceAr NFM:11,FiraCode Nerd Font:h12"
   vim.g.neovide_cursor_vfx_mode = "sonicboom"
 end
 
--- Command for neovide remote
+-- Command for Neovide remote
 vim.cmd("command! -nargs=0 SetRemoteConfig set guifont=MonaspiceAr\\ Nerd\\ Font\\ Mono:h12")
+
