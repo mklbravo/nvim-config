0625df22695ef1c428c44c660c236f729b55e986
Mikel Bravo <me@mklbravo.net>
2026-01-22 12:25:28 +0100

    refactor(snacks): migrate fuzzy finder from telescope to snacks and update keybindings
    - Remove telescope plugin and its configuration
    - Add snacks.nvim picker keybindings for file, buffer, git, and LSP
    navigation
    
    This refactor replaces telescope.nvim with snacks.nvim as the primary
    fuzzy finder, updating all relevant keybindings and removing the old
    plugin configuration.

--- lua/plugins/snacks/picker.lua
+++ lua/plugins/snacks/picker.lua
@@ -10,5 +10,93 @@ return {
     enabled = true,
     sources = {},
   },
-  keys = {},
+  keys = {
+    -- Top Pickers & Explorer
+    {
+      "<leader><space>",
+      function()
+        Snacks.picker.smart()
+      end,
+      desc = "Smart Find Files",
+    },
+    {
+      "<leader>,",
+      function()
+        Snacks.picker.buffers()
+      end,
+      desc = "Buffers",
+    },
+    {
+      "<leader>/",
+      function()
+        Snacks.picker.grep()
+      end,
+      desc = "Grep",
+    },
+    {
+      "<leader>:",
+      function()
+        Snacks.picker.command_history()
+      end,
+      desc = "Command History",
+    },
+    {
+      "<leader>n",
+      function()
+        Snacks.picker.notifications()
+      end,
+      desc = "Notification History",
+    },
+    {
+      "<leader>fb",
+      function()
+        Snacks.picker.buffers()
+      end,
+      desc = "Buffers",
+    },
+    {
+      "<leader>ff",
+      function()
+        Snacks.picker.files()
+      end,
+      desc = "Find Files",
+    },
+    {
+      "<leader>fg",
+      function()
+        Snacks.picker.git_files()
+      end,
+      desc = "Find Git Files",
+    },
+    {
+      "<leader>fp",
+      function()
+        Snacks.picker.projects()
+      end,
+      desc = "Projects",
+    },
+    {
+      "<leader>fr",
+      function()
+        Snacks.picker.recent()
+      end,
+      desc = "Recent",
+    },
+    -- git
+    {
+      "<leader>gl",
+      function()
+        Snacks.picker.git_log()
+      end,
+      desc = "Git Log",
+    },
+    -- Buffer Pickers
+    {
+      "<leader>bs",
+      function()
+        Snacks.picker.lsp_symbols()
+      end,
+      desc = "LSP Symbols",
+    },
+  },
 }
